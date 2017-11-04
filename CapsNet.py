cdd0d830f98af4c8ec368d8f6f568df84bee3ebf
Sahil Rajesh Dhayalkar <sdhayalk@asu.edu>
2017-11-03 17:14:18 -0700

    modified get_dataset function

--- CapsNet.py
+++ CapsNet.py
@@ -1,10 +1,12 @@
-import nunmpy as np
+import numpy as np
 
 from data_preprocessing import get_dataset_features
 
 TRAIN_FOLDER_PATH = "G:/DL/Iceberg Classifier Challenge/train/data/processed/train.json"
-TEST_FOLDER_PATH = "G:/DL/Iceberg Classifier Challenge/test/data/processed/test.json"
+# TEST_FOLDER_PATH = "G:/DL/Iceberg Classifier Challenge/test/data/processed/test.json"
 
 dataset_train_features, dataset_train_labels = get_dataset_features(TRAIN_FOLDER_PATH, labels_available=True)
-dataset_test_features = get_dataset_features(TEST_FOLDER_PATH, labels_available=False)
+# dataset_test_features = get_dataset_features(TEST_FOLDER_PATH, labels_available=False)
 
+print(dataset_train_features.shape)
+print(dataset_train_labels.shape)
--- data_preprocessing.py
+++ data_preprocessing.py
@@ -3,22 +3,32 @@ import pandas as pd
 import os
 
 def get_dataset_features(path, labels_available=False):
-	train = pd.read_json(path)
-	train_id = train.id.values
-	train_band_1 = train.band_1.values
-	train_band_2 = train.band_2.values
-	train_label = train.is_iceberg.values
+	data = pd.read_json(path)
+	data_id = data.id.values
+	data_band_1 = data.band_1.values
+	data_band_2 = data.band_2.values
+	data_label = data.is_iceberg.values
 
 	dataset_features = []
-	dataset_features.append(train_band_1)
-	dataset_features.append(train_band_2)
+	for i in range(0, data_band_1.shape[0]):
+		temp = []
+		temp.append(np.array(data_band_1[i]).reshape((75,75)))
+		temp.append(np.array(data_band_2[i]).reshape((75,75)))
+		temp = np.array(temp)
+		temp = temp.reshape((75,75,2))
+		dataset_features.append(temp)
+
 	dataset_features = np.array(dataset_features)
 
 	if labels_available:
 		dataset_labels = []
-		dataset_labels.append(train_label)
+		
+		for i in range(0, data_band_1.shape[0]):
+			temp = np.zeros(2)
+			temp[data_label[i]] = 1
+			dataset_labels.append(temp)
+		
 		dataset_labels = np.array(dataset_labels)
-
 		return dataset_features, dataset_labels
 
 	return dataset_features
